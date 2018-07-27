7773152804666c9527f7b935daeb35f8d774f40b
unknown <S_Gravesen92@hotmail.com>
2018-07-27 23:02:55 +0200

    Solved algo 2 of 100 Algorithms Challenge

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,19 @@
 function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
+  let total = 0;
+  param1.map((num) => {
+    total += num;
+  });
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+  return total;
+}
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
