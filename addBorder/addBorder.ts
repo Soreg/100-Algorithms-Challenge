0273fae330587f4783e1087a43f71b678a37eac6
unknown <S_Gravesen92@hotmail.com>
2018-07-27 23:17:39 +0200

    Solved Algo 3 of 100 Algorithms Challenge

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[]): any {
+  const wall = '*'.repeat(picture[0].length + 2);
 
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = '*'.concat(picture[i], '*');
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
