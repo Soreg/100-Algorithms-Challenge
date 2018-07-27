be88e2622824feb3d4ad65e02554e6b7e0b9a60b
unknown <S_Gravesen92@hotmail.com>
2018-07-27 22:44:56 +0200

    Initial commit

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,7 @@
+function absoluteValuesSumMinimization(a: number[]): number {
+}
+
+// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
