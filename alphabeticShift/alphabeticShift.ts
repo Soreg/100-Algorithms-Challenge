f4cd5e1c8bb31cdb150f24283d9296f6ca8f8a07
Søren Gravesen <grav@hesehus.dk>
2018-07-31 12:14:25 +0200

    Solved Algo 9 of 100 algorithms challenge

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,12 @@
 function alphabeticShift(inputString: string): string {
+  const alphabet = ('abcdefghijklmnopqrstuvwxyz').split('')
 
+  const string = inputString.split('').map((char) => {
+    const i = alphabet.indexOf(char);
+    return alphabet[i+1] !== undefined ? alphabet[i+1] : 'a'
+  }).join('');
+
+  return string;
 }
 
 console.log(alphabeticShift('crazy'));
