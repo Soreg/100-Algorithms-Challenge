119c00312e1c0de79ed831ce87088021311418d9
Søren Gravesen <grav@hesehus.dk>
2018-07-31 15:01:13 +0200

    Solved Algo 10 of 100 algorithms challenge

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 function alternatingSums(a: number[]): number[] {
+  let team1 = [];
+  let team2 = [];
+  let allTeams = [];
 
+  a.map((num, i) => {
+    i % 2 === 0 ? team1.push(num) : team2.push(num);
+  });
+
+  allTeams.push(team1.reduce((a, b) => a + b), team2.reduce((a, b) => a + b));
+
+  return allTeams;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
