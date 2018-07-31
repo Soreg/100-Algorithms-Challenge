6e597cd66214db3bc2680c51e3db00c351dcf8d9
Søren Gravesen <grav@hesehus.dk>
2018-07-31 13:15:03 +0200

    Solved Algo 8 of 100 algorithms challenge

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,24 @@
 function alphabetSubsequence(s: string): boolean {
+  const charCodes = s.split('');
+  const chars = [];
 
+  charCodes.forEach((char) => {
+    chars.push(char.charCodeAt(0));
+  })
+
+  if (new Set(chars).size !== chars.length) {
+    return false;
+  }
+
+  for(let i = 0; i < chars.length; i++) {
+    if(chars[i] >= chars[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+
+  
 }
 
 console.log(alphabetSubsequence('zab'))
