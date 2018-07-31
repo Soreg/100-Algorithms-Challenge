ec563b06533afbd8e439a0ca2dd0f6571c69967c
Søren Gravesen <grav@hesehus.dk>
2018-07-31 12:03:38 +0200

    Solved Algo 7 of 100 algorithms challenge

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,21 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  let count = 0;
 
+  sequence.map((num, i) => {
+    if(num <= sequence[i-1]) {
+      count++;
+      if(num <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) {
+        return false;
+      }
+    }
+  })
+  
+  return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+// find first wrong and remove
+// If the soreted version is equal to original, then true
+// else falls
+
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
